d7dbb4e7623f1aaea1d65a1221674e82a7fef92b
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-04-30 23:55:41 +0200

    further creation of form

--- src/app/adding/page.tsx
+++ src/app/adding/page.tsx
@@ -1,23 +1,41 @@
+"use client";
+
 import Card from "@/components/Card";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
 
 export default function AddingOffer({}: Props) {
+  const [title, setTitle] = useState("");
+  const [isTitleCorrect, setIsTitleCorrect] = useState(true);
+
+  useEffect(() => {
+    if (title.length >= 16 || title.length == 0) {
+      setIsTitleCorrect(true);
+    } else {
+      setIsTitleCorrect(false);
+    }
+    console.log("use effect");
+  }, [title]);
+
   return (
     <>
-      Dodaj ogłoszenie
+      <p className="text-2xl font-bold">Dodaj ogłoszenie</p>
       <Card>
-        <p>Im więcej szczegółów, tym lepiej!</p>
+        <p className=" font-bold">Im więcej szczegółów, tym lepiej!</p>
         <p>Tytuł ogłoszenia*</p>
         <input
           type="text"
           name=""
           id=""
-          className="border-b-4 bg-gray-200 p-4"
+          className={`border-b-4 bg-gray-200 p-4 ${
+            isTitleCorrect ? "" : "bg-red-300"
+          }`}
           placeholder="np. iPhone 11 na gwarancji"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        wpisz przynajmniej 16 znaków
+        <p className="text-xs">wpisz przynajmniej 16 znaków</p>
       </Card>
       <Card>
         Zdjęcia Pierwsze zdjęcie będzie zdjęciem głównym. <br />
@@ -35,8 +53,18 @@ export default function AddingOffer({}: Props) {
         ></textarea>
       </Card>
       <Card>Lokalizacja</Card>
-      <Card>Dane kontaktowe</Card>
-      <Card>guziki</Card>
+      <Card>
+        <p>Dane kontaktowe</p>
+        <p>Osoba kontaktowa*</p>
+        <p>Jerzy</p>
+        <p>Adres e-mail</p>
+        <p>jerzy@jerzy.jerzy</p>
+        <p>Numer telefonu</p>
+        <p>123 123 123</p>
+      </Card>
+      <Card>
+        <button className="ml-auto py-2 px-4 bg-emerald-950 text-white">Dodaj ogłoszenie</button>
+      </Card>
     </>
   );
 }
