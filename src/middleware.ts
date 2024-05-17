165c7334f9d19e642033a2956d2e0695b331a3b5
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2024-05-17 16:44:36 +0200

    some thing

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+// export function middleware(request: NextRequest) {
+//     return NextResponse.redirect(new URL("/", request.url))
+// }
+
+// export const config ={
+//     matcher: '/profile'
+// }
+
+
+export function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname === '/profile') {
+        return NextResponse.redirect(new URL("/", request.url))
+    }
+}
